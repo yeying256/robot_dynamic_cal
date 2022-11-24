f2f7ad2ae1592270be20ee64980ec9ab2032d625
yeying256 <2569209027@qq.com>
2022-11-24 20:56:09 +0800

    20221124

---
+++ include/franka_impedance_param.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <Eigen/Eigen>
+#include <ros/ros.h>
+
+namespace xjtu_dynamic_ns
+{
+
+    
+
+
+    class franka_impedance_param
+    {
+    private:
+        /* data */
+    public:
+        franka_impedance_param();
+        ~franka_impedance_param();
+    };
+
+
+}
---
+++ src/franka_impedance_param.cpp
@@ -0,0 +1,17 @@
+#include "franka_impedance_param.h"
+
+namespace xjtu_dynamic_ns
+{
+
+    
+    franka_impedance_param::franka_impedance_param()
+    {
+        ;
+    }
+    
+    franka_impedance_param::~franka_impedance_param()
+    {
+        ;
+    }
+
+}
