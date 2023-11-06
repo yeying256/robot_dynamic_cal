90b545aefb00155e9e7cff6fc6e383c8475cc862
usmc123 <2569209027@qq.com>
2023-11-06 09:44:22 +0800

    更新了pid的最大限制

--- include/PID_controller.h
+++ include/PID_controller.h
@@ -16,9 +16,11 @@ namespace xj_dy_ns
         double m_last_err;
         double m_i_err;
         double m_d_err;
+        double max_output_;
     public:
         PID_controller(/* args */);
         void init(double p, double i, double d);
+        void init(double p, double i, double d,double max_output);
         ~PID_controller();
         PID_controller(double p, double i, double d);
         double PID(double err);
--- src/PID_controller.cpp
+++ src/PID_controller.cpp
@@ -9,12 +9,20 @@ namespace xj_dy_ns
         m_last_err = 0;
         m_i_err = 0;
         m_d_err = 0;
+        max_output_ = 0.1;
     }
     
     PID_controller::~PID_controller()
     {
     }
 
+    /**
+     * @brief 最大输出默认0.1
+     * 
+     * @param p 
+     * @param i 
+     * @param d 
+     */
     void PID_controller::init(double p, double i, double d)
     {
         m_p=p;
@@ -23,8 +31,35 @@ namespace xj_dy_ns
         m_last_err = 0;
         m_i_err = 0;
         m_d_err = 0;
+        max_output_ = 0.1;
     }
 
+    /**
+     * @brief 加入了maxoutput的初始化函数
+     * 
+     * @param p 
+     * @param i 
+     * @param d 
+     * @param max_output 限制最大输出
+     */
+    void PID_controller::init(double p, double i, double d,double max_output)
+    {
+        m_p=p;
+        m_i=i;
+        m_d=d;
+        m_last_err = 0;
+        m_i_err = 0;
+        m_d_err = 0;
+        max_output_ = max_output;
+    }
+
+
+    /**
+     * @brief pid计算
+     * 
+     * @param err 
+     * @return double 输出pid的计算值 
+     */
     double PID_controller::PID(double err)
     {
         m_i_err += m_last_err;
@@ -32,7 +67,7 @@ namespace xj_dy_ns
         double k = m_p*err + m_i*m_i_err + m_d*m_d_err;
         m_last_err = err;
         // if(fabs(k) < 0.0001) k = fabs(k) / k * 0.01;
-        if(fabs(k) > 0.1 ) k = fabs(k) / k * 0.1;
+        if(fabs(k) > max_output_ ) k = fabs(k) / k * max_output_;
         return k;
     }
 
